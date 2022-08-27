3634d1595f967527318c1f432539388b1c165627
pullerize <pullerize@gmail.com>
2022-08-27 12:33:40 +0300

    ConditionifElse project added

---
+++ Examples005_ConditioniflEse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if  (username.ToLower == "Маша")
+    {
+        Console.WriteLine("Здаров Маша"); 
+    }
+else 
+    {
+        Console.Write("Привет ");
+        Console.WriteLine(username);
+    }
+
+
