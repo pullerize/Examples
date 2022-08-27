2f8d01ea4b10c28135100e9d1291970702eea08a
pullerize <pullerize@gmail.com>
2022-08-27 12:45:23 +0300

    Base project added

---
+++ Examples006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 5;
+int c = 9;
+int d = 6;
+int f = 7;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (f > max) max = f;
+
+Console.Write("max = ");
+Console.WriteLine(max);
